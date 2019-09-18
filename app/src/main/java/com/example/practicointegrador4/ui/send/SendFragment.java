8f6aa92702d9330ea6f2002526f8a3ab2a6cd130
Pedro <pedro.maximilian@gmail.com>
2019-09-18 18:31:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/practicointegrador4/ui/send/SendFragment.java
@@ -0,0 +1,42 @@
+package com.example.practicointegrador4.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.practicointegrador4.R;
+
+public class SendFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+
+        Button boton = root.findViewById(R.id.btn);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.enviado); // Enviado es el fragmento de destino
+
+
+            }
+        });
+
+
+        return root;
+    }
+}
